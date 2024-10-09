c5beb801a6b9c3ed207049ef35a5c9069c146d9d
KaiqueVA <kaique_x_@hotmail.com>
2024-10-09 14:02:55 -0300

    Refactor URLs configuration in urls.py

--- contentor_api/contentors/urls.py
+++ contentor_api/contentors/urls.py
@@ -1,8 +1,19 @@
 # contentors/urls.py
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContentorViewSet
+
+from .views.contentor_views import ContentorViewSet, LastContentorViewSet
+from .views.gps_views import ContentorGPSViewSet, LastContentorGPSViewSet, ContentorGoogleMapsViewSet
+# from .views import ContentorViewSet, ContentorGPSViewSet, LastContentorGPSViewSet, LastContentorViewSet, ContentorGoogleMapsViewSet
 
 router = DefaultRouter()
 router.register(r'contentors', ContentorViewSet)
+router.register(r'last_contentor', LastContentorViewSet, basename='last-contentor')
+router.register(r'contentors_gps', ContentorGPSViewSet, basename='contentors-gps')
+router.register(r'last_contentor_gps', LastContentorGPSViewSet, basename='last-contentor-gps')
+router.register(r'google_maps', ContentorGoogleMapsViewSet, basename='contentor-google-maps')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
