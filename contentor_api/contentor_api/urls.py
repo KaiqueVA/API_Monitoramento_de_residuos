0a120863aa7f7ab4e0340d8aca11072f29064a75
KaiqueVA <kaique_x_@hotmail.com>
2024-10-09 14:02:26 -0300

    Refactor Swagger configuration in urls.py

--- contentor_api/contentor_api/urls.py
+++ contentor_api/contentor_api/urls.py
@@ -5,11 +5,14 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Configuração do Swagger
 schema_view = get_schema_view(
    openapi.Info(
       title="Contentor API",
       default_version='v1',
-      description="API for managing contentors",
+      description="API para gerenciar contentores",
+      terms_of_service="https://www.youtube.com/watch?v=tvkxupwbFLk",
+      
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -17,7 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('contentors.urls')),  # Rotas da API contentors
+    path('api/', include('contentors.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
