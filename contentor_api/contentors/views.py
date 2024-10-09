286f1ab705284660f1ccd44bff6d5e405caf0767
KaiqueVA <kaique_x_@hotmail.com>
2024-10-09 14:02:35 -0300

    Remove ContentorViewSet from views.py

--- contentor_api/contentors/views.py
+++
@@ -1,8 +0,0 @@
-# contentors/views.py
-from rest_framework import viewsets
-from .models import Contentor
-from .serializers import ContentorSerializer
-
-class ContentorViewSet(viewsets.ModelViewSet):
-    queryset = Contentor.objects.all()
-    serializer_class = ContentorSerializer
