aa6e0a3a2179de4a7bae501a1d733e5da94b9562
KaiqueVA <kaique_x_@hotmail.com>
2024-10-10 10:55:58 -0300

    feat: add volume views to contentor API

--- contentor_api/contentors/views/__init__.py
+++ contentor_api/contentors/views/__init__.py
@@ -1,10 +1,15 @@
 from .contentor_views import ContentorViewSet
 from .gps_views import ContentorGPSViewSet, LastContentorGPSViewSet, ContentorGoogleMapsViewSet
 from .battery_views import BatteryViewSet, LastBatteryViewSet
+from .volume_views import VolumeViewSet, LastVolumeViewSet
 
 __all__ = [
     "ContentorViewSet",
     "ContentorGPSViewSet",
     "LastContentorGPSViewSet",
-    "ContentorGoogleMapsViewSet"
+    "ContentorGoogleMapsViewSet",
+    
+    "VolumeViewSet",
+    "LastVolumeViewSet",
+    
 ]
