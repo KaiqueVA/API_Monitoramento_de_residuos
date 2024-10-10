eaacba10d43f655c5a8af08c25770eb15147bfef
KaiqueVA <kaique_x_@hotmail.com>
2024-10-10 10:50:02 -0300

    feat: add battery views to contentor API

--- contentor_api/contentors/views/__init__.py
+++ contentor_api/contentors/views/__init__.py
@@ -1,5 +1,6 @@
 from .contentor_views import ContentorViewSet
 from .gps_views import ContentorGPSViewSet, LastContentorGPSViewSet, ContentorGoogleMapsViewSet
+from .battery_views import BatteryViewSet, LastBatteryViewSet
 
 __all__ = [
     "ContentorViewSet",
