1a9ebda554921f277d01fda61726de0677e6b834
KaiqueVA <kaique_x_@hotmail.com>
2024-11-12 11:46:30 -0300

    feat: add custom pagination for ContentorViewSet

--- contentor_api/contentors/views/contentor_views.py
+++ contentor_api/contentors/views/contentor_views.py
@@ -1,16 +1,19 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from ..models import Contentor
 from ..serializers import ContentorSerializer
 
 
-
-
+class ContentorPagination(PageNumberPagination):
+    page_size = 10  # Número de itens por página
+    page_size_query_param = 'page_size'  # Permite que o cliente defina o tamanho da página
+    max_page_size = 100  # Tamanho máximo da página que o cliente pode pedir
 
 class ContentorViewSet(viewsets.ModelViewSet):
     queryset = Contentor.objects.all()
     serializer_class = ContentorSerializer
-    
+    pagination_class = ContentorPagination
     
 class LastContentorViewSet(viewsets.ViewSet):
     def list(self, request):
