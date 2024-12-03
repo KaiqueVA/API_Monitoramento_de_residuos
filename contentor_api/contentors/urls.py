d068299cc00934785117e6b0c75f33439d81024d
KaiqueVA <kaique_x_@hotmail.com>
2024-12-03 12:03:33 -0300

    feat: update URL routing to include DateRangeViewSets for Contentor, Volume, GPS, and Battery

--- contentor_api/contentors/urls.py
+++ contentor_api/contentors/urls.py
@@ -2,14 +2,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views.contentor_views import ContentorViewSet, LastContentorViewSet, TodayContentorViewSet, FilteredContentorViewSet
-from .views.gps_views import ContentorGPSViewSet, LastContentorGPSViewSet, ContentorGoogleMapsViewSet, GPSByDayViewSet
-from .views.battery_views import BatteryViewSet, LastBatteryViewSet, BatteryByDayViewSet
-from .views.volume_views import VolumeViewSet, LastVolumeViewSet, VolumeByDayViewSet
+from .views.contentor_views import ContentorViewSet, LastContentorViewSet, TodayContentorViewSet, ContentorByDateRangeViewSet
+from .views.gps_views import ContentorGPSViewSet, LastContentorGPSViewSet, ContentorGoogleMapsViewSet, GPSByDateRangeViewSet
+from .views.battery_views import BatteryViewSet, LastBatteryViewSet, BatteryByDateRangeViewSet
+from .views.volume_views import VolumeViewSet, LastVolumeViewSet, VolumeByDateRangeViewSet
 # from .views import ContentorViewSet, ContentorGPSViewSet, LastContentorGPSViewSet, LastContentorViewSet, ContentorGoogleMapsViewSet
 
 router = DefaultRouter()
-router.register(r'contentors', ContentorViewSet)
+router.register(r'Contentors', ContentorViewSet, basename='Contentors')
 router.register(r'last_contentor', LastContentorViewSet, basename='last-contentor')
 router.register(r'today_contentor', TodayContentorViewSet, basename='today-contentor')
 router.register(r'contentors_gps', ContentorGPSViewSet, basename='contentors-gps')
@@ -19,10 +19,10 @@ router.register(r'contentors_batteries', BatteryViewSet, basename='contentor-bat
 router.register(r'last_battery', LastBatteryViewSet, basename='last-battery')
 router.register(r'contentors_volumes', VolumeViewSet, basename='contentor-volumes')
 router.register(r'last_volume', LastVolumeViewSet, basename='last-volume')
-router.register(r'filtered-contentores', FilteredContentorViewSet, basename='filtered-contentor')
-router.register(r'volume_by_day', VolumeByDayViewSet, basename='volume-by-day')
-router.register(r'gps_by_day', GPSByDayViewSet, basename='gps-by-day')
-router.register(r'battery_by_day', BatteryByDayViewSet, basename='battery-by-day')
+router.register(r'Contentor-Date-Range', ContentorByDateRangeViewSet, basename='Contentor-Date-Range')
+router.register(r'Volume-Date-Range', VolumeByDateRangeViewSet, basename='Volume-Date-Range')
+router.register(r'GPS-Date-Range', GPSByDateRangeViewSet, basename='GPS-Date-Range')
+router.register(r'Battery_Date_Range', BatteryByDateRangeViewSet, basename='Battery-Date-Range')
 
 
 
