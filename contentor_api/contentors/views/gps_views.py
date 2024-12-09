3571226371fbc90dc774e89eb3c548f087c70a20
KaiqueVA <kaique_x_@hotmail.com>
2024-12-09 14:30:22 -0300

    feat: add GPSLongerDistanceLocationViewSet to calculate and return the farthest GPS coordinates

--- contentor_api/contentors/views/gps_views.py
+++ contentor_api/contentors/views/gps_views.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from ..views.base_views import BaseDateRangeViewSet
+from ..utils.utils import encontrar_posicao_mais_longe
 
 class ContentorGPSPagination(PageNumberPagination):
     page_size = 10
@@ -86,3 +87,40 @@ class GPSByDateRangeViewSet(BaseDateRangeViewSet):
     def list(self, request):
         self.data_fields = ['id', 'latitude', 'longitude', 'timestamp']
         return super().list(request)
+    
+
+
+
+# ViewSet para calcular a posição mais distante com detalhes
+class GPSLongerDistanceLocationViewSet(viewsets.ViewSet):
+    """
+    Calcula a posição mais distante entre várias coordenadas fornecidas e retorna detalhes.
+    """
+
+    def list(self, request):
+        try:
+            # Obtendo os dados GPS do banco de dados
+            gps_data = Contentor.objects.values('id', 'latitude', 'longitude', 'timestamp')
+            com_detalhes = [
+                (data['latitude'], data['longitude'], data['id'], data['timestamp'])
+                for data in gps_data if data['latitude'] and data['longitude']
+            ]
+
+            if not com_detalhes:
+                return Response({"detail": "Nenhuma coordenada encontrada."}, status=status.HTTP_404_NOT_FOUND)
+
+            # Calculando a posição mais distante
+            posicao_mais_longe = encontrar_posicao_mais_longe(com_detalhes)
+
+            if posicao_mais_longe:
+                return Response(
+                    posicao_mais_longe, status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {"detail": "Não foi possível calcular a posição mais distante."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        except Exception as e:
+            return Response({"erro": str(e)}, status=status.HTTP_400_BAD_REQUEST)
